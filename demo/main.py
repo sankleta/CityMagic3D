fix scores calculation

demo/main.py
@@ -1,14 +1,20 @@
 import numpy as np
 import pyvista as pv
 
-from instance_masks_from_images import clip
+from instance_masks_from_images import image_text
 
 point_cloud = pv.read(r"C:\Users\sankl\Downloads\WMSC\WMSC_points.ply")
+loaded_masks = np.load(r'C:\Users\sankl\PycharmProjects\CityMagic3D\instance_masks_from_images\outputs\instance_masks_from_images\2024-05-19_21-46-05\DJI_0887.JPG__mask_indices.npz')
+loaded_text = np.load(r'C:\Users\sankl\PycharmProjects\CityMagic3D\instance_masks_from_images\outputs\instance_masks_from_images\2024-05-19_21-46-05\DJI_0887.JPG__mask_text_embeddings.npz')
 
+
+mask_embeddings = {key: loaded_masks[key] for key in loaded_masks}
+mask_text_embeddings = {key: loaded_text[key].reshape(768,) for key in loaded_text}
 query = "vegetation"
-clip.load_clip("google/siglip-base-patch16-224")
-qe = clip.get_query_embedding(clip, query)
+model = image_text.load_image_text_model("google/siglip-base-patch16-224")
+qe = image_text.get_query_embedding(model, query)
 
+scores = image_text.compute_cosine_similarity_scores(mask_text_embeddings, qe)
 
 
 # Create a plotter object

instance_masks_from_images/image_text.py
@@ -2,6 +2,7 @@ import numpy as np
 import torch
 from transformers import AutoProcessor, SiglipModel
 
+
 from instance_masks_from_images.utils import mask_and_crop_image
 
 DEVICE = torch.device('cuda' if torch.cuda.is_available() else "cpu")
@@ -23,22 +24,17 @@ def extract_text_features(image_text_model, image, mask):
 
 
 def get_query_embedding(image_text_model, text_query):
-    text_input_processed = image_text_model["processor"].tokenize(text_query).to(DEVICE)
+    text_input_processed = image_text_model["processor"](text=text_query, padding="max_length", return_tensors="pt").to(
+        DEVICE)
     with torch.no_grad():
-        sentence_embedding = image_text_model["model"].encode_text(text_input_processed)
+        sentence_embedding = image_text_model["model"].get_text_features(**text_input_processed)
 
-    sentence_embedding_normalized = (
-            sentence_embedding / sentence_embedding.norm(dim=-1, keepdim=True)).float().cpu()
-    return sentence_embedding_normalized.squeeze().numpy()
+    return sentence_embedding.squeeze().cpu()
 
 
-def compute_similarity_scores(mask_embedding, query_embedding):
-    scores = np.zeros(len(mask_embedding))
-    for mask_idx, mask_emb in enumerate(mask_embedding):
-        mask_norm = np.linalg.norm(mask_emb)
-        if mask_norm < 0.001:
-            continue
-        normalized_emb = (mask_emb / mask_norm)
-        scores[mask_idx] = normalized_emb @ query_embedding
-
+def compute_cosine_similarity_scores(mask_embeddings, query_embedding):
+    cossim = torch.nn.CosineSimilarity(dim=0, eps=1e-3)
+    scores = np.zeros(len(mask_embeddings))
+    for i, mask_idx in enumerate(mask_embeddings):
+        scores[i] = cossim(torch.from_numpy(mask_embeddings[mask_idx]), query_embedding)
     return scores

instance_masks_from_images/main.py
@@ -51,7 +51,7 @@ def get_indices_on_point_cloud(resized_resolution, projected_points, visibility_
     return np.where(all_points_mask)[0]
 
 
-@hydra.main(version_base="1.3", config_path=".", config_name="config.yaml")
+@hydra.main(version_base="1.3", config_path=".", config_name="config2.yaml")
 def main(cfg: DictConfig):
     # Load the SAM model
     sam_mask_generator = load_sam_mask_generator(cfg)
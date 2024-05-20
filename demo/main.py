e5020b2cad50ceda8cedb6b8cf63fdec65684f5f
Tanya Kozel <sankleta@gmail.com>
2024-05-20 23:33:45 +0200

    prettify

--- demo/main.py
+++ demo/main.py
@@ -4,19 +4,17 @@ import pyvista as pv
 from demo.utils import show_top_20_instances, show_heatmap
 from instance_masks_from_images import image_text
 
-# point_cloud = pv.read(r"C:\Users\sankl\Downloads\WMSC\WMSC_points.ply")
-# loaded_masks = np.load(r'C:\Users\sankl\PycharmProjects\CityMagic3D\instance_masks_from_images\outputs\instance_masks_from_images\2024-05-19_21-46-05\DJI_0887.JPG__mask_indices.npz')
-# loaded_text = np.load(r'C:\Users\sankl\PycharmProjects\CityMagic3D\instance_masks_from_images\outputs\instance_masks_from_images\2024-05-19_21-46-05\DJI_0887.JPG__mask_text_embeddings.npz')
+TEXT_EMBEDDING_SIZE = 768
 
-mesh = pv.read(r"C:\Users\sankl\Downloads\RA\RA_1M.ply")
-loaded_masks = np.load(r'C:\Users\sankl\Downloads\merged_masks.npz')
-loaded_text = np.load(r'C:\Users\sankl\Downloads\merged_embeddings.npz')
+MESH = pv.read(r"C:\Users\sankl\Downloads\RA\RA_1M.ply")
+LOADED_MASKS = np.load(r'C:\Users\sankl\Downloads\merged_masks.npz')
+LOADED_TEXT_EMBEDDINGS = np.load(r'C:\Users\sankl\Downloads\merged_embeddings.npz')
 
 QUERY = "green"
 
-mask_embeddings = {key: loaded_masks[key] for key in loaded_masks}
+mask_embeddings = {key: LOADED_MASKS[key] for key in LOADED_MASKS}
 
-mask_text_embeddings = {key: loaded_text[key].reshape(768, ) for key in loaded_text}
+mask_text_embeddings = {key: LOADED_TEXT_EMBEDDINGS[key].reshape(TEXT_EMBEDDING_SIZE, ) for key in LOADED_TEXT_EMBEDDINGS}
 
 model = image_text.load_image_text_model("google/siglip-base-patch16-224")
 query_embeddings = image_text.get_query_embedding(model, QUERY)
@@ -28,11 +26,11 @@ plotter = pv.Plotter()
 plotter.add_text(f"Query: '{QUERY}'", font_size=14)
 
 # Add the mesh to the plotter
-plotter.add_mesh(mesh, scalars='RGBA', rgb=True)
+plotter.add_mesh(MESH, scalars='RGBA', rgb=True)
 
 # Add masks as points on top
 # show_top_20_instances(plotter, mesh, scores, mask_embeddings)
-show_heatmap(plotter, mesh, scores, mask_embeddings)
+show_heatmap(plotter, MESH, scores, mask_embeddings)
 
 # Display the plot
 plotter.show()

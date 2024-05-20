531dc12915fe2f533f7ebf7e5e8194484f3186f8
Tanya Kozel <sankleta@gmail.com>
2024-05-20 23:13:20 +0200

    get heatmap

--- demo/main.py
+++ demo/main.py
@@ -2,10 +2,10 @@ import numpy as np
 import pyvista as pv
 
 import demo.quering
-from demo.utils import show_top_20_instances
+from demo.utils import show_top_20_instances, show_heatmap
 from instance_masks_from_images import image_text
 
-#point_cloud = pv.read(r"C:\Users\sankl\Downloads\WMSC\WMSC_points.ply")
+# point_cloud = pv.read(r"C:\Users\sankl\Downloads\WMSC\WMSC_points.ply")
 # loaded_masks = np.load(r'C:\Users\sankl\PycharmProjects\CityMagic3D\instance_masks_from_images\outputs\instance_masks_from_images\2024-05-19_21-46-05\DJI_0887.JPG__mask_indices.npz')
 # loaded_text = np.load(r'C:\Users\sankl\PycharmProjects\CityMagic3D\instance_masks_from_images\outputs\instance_masks_from_images\2024-05-19_21-46-05\DJI_0887.JPG__mask_text_embeddings.npz')
 
@@ -13,23 +13,24 @@ mesh = pv.read(r"C:\Users\sankl\Downloads\RA\RA_1M.ply")
 loaded_masks = np.load(r'C:\Users\sankl\Downloads\merged_masks.npz')
 loaded_text = np.load(r'C:\Users\sankl\Downloads\merged_embeddings.npz')
 
+
+QUERY = "green"
+
 mask_embeddings = {key: loaded_masks[key] for key in loaded_masks}
 
 mask_text_embeddings = {key: loaded_text[key].reshape(768, ) for key in loaded_text}
-query = "green"
 model = image_text.load_image_text_model("google/siglip-base-patch16-224")
-query_embeddings = demo.quering.get_query_embedding(model, query)
+query_embeddings = demo.quering.get_query_embedding(model, QUERY)
 
 scores = demo.quering.compute_cosine_similarity_scores(mask_text_embeddings, query_embeddings)
 
 # Create a plotter object
 plotter = pv.Plotter()
-plotter.add_camera_orientation_widget()
+plotter.add_text(f"Query: '{QUERY}'", font_size=14)
 
 # Add the point cloud or mesh to the plotter
 plotter.add_mesh(mesh, scalars='RGBA', rgb=True)
-plotter.set_color_cycler(['blue', 'green', 'red', 'cyan', 'magenta', 'yellow', 'black', 'purple', 'pink', 'brown', 'orange', 'teal', 'coral', 'lightblue', 'lime', 'lavender', 'turquoise', 'darkgreen', 'tan', 'salmon', 'gold', 'lightpurple', 'darkred', 'darkblue'])
-show_top_20_instances(plotter, mesh, scores, mask_embeddings)
-
+#show_top_20_instances(plotter, mesh, scores, mask_embeddings)
+show_heatmap(plotter, mesh, scores, mask_embeddings)
 # Display the plot
 plotter.show()
--- demo/utils.py
+++ demo/utils.py
@@ -1,12 +1,37 @@
 import heapq
 
+COLORS = ['darkred', 'brown', 'red', 'salmon', 'coral', 'orange', 'gold', 'yellow', 'lime', 'green', 'darkgreen',
+          'teal', 'turquoise', 'cyan', 'lightblue', 'blue', 'darkblue', 'purple', 'lightpurple', 'lavender', 'magenta',
+          'pink']
 
-def show_heatmap(scores):
-    pass
+SCALE_COLORS = colors = [
+    "#FF0000",  # Red
+    "#FF7F00",  # Orange
+    "#FFFF00",  # Yellow
+    "#7FFF00",  # Light Green
+    "#00FF00",  # Green
+    "#00FF7F",  # Spring Green
+    "#00FFFF",  # Cyan
+    "#007FFF",  # Azure
+    "#0000FF",  # Blue
+    "#7F00FF"  # Violet
+]
 
 
-def show_instances(scores):
-    pass
+def show_heatmap(plotter, mesh, scores, masks):
+    plotter.add_text("Showing heatmap in rainbow style, from red (the lowest score) to violet (the highest score)", font_size=12, position="lower_left")
+    clean(masks, scores)
+    for i in masks:
+        if scores[i] > 0:
+            plotter.add_points(mesh.points[masks[i], :], color=get_color(scores[i]), render_points_as_spheres=True,
+                               point_size=3, opacity=scores[i])
+
+
+def get_color(score):
+    # Calculate the index in the color list. Assuming the score range is from 0 to 0.5
+    normalized_score = score / 0.5
+    index = int(normalized_score * (len(colors) - 1))
+    return colors[index]
 
 
 def clean(dict1, dict2):
@@ -17,14 +42,12 @@ def clean(dict1, dict2):
         del dict2[key]
 
 
-def show_top_20_instances(plotter, point_cloud, scores, masks):
-    plotter.set_color_cycler(
-        ['blue', 'green', 'red', 'cyan', 'magenta', 'yellow', 'black', 'purple', 'pink', 'brown', 'orange', 'teal',
-         'coral', 'lightblue', 'lime', 'lavender', 'turquoise', 'darkgreen', 'tan', 'salmon', 'gold', 'lightpurple',
-         'darkred', 'darkblue'])
+def show_top_20_instances(plotter, mesh, scores, masks):
+    plotter.add_text("Showing top 20 matching instances", font_size=12, position="lower_left")
+    plotter.set_color_cycler(COLORS)
     clean(masks, scores)
     top_20 = heapq.nlargest(20, scores, key=scores.get)
 
     for i in top_20:
-        plotter.add_points(point_cloud.points[masks[i], :], render_points_as_spheres=True,
+        plotter.add_points(mesh.points[masks[i], :], render_points_as_spheres=True,
                            point_size=3)

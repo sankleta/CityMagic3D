b6892ea4237ae5da710fbe8b859bead611a6c4ac
Tanya Kozel <sankleta@gmail.com>
2024-05-26 13:28:06 +0200

    add fmow data preprocessing

--- demo/main.py
+++ demo/main.py
@@ -10,10 +10,12 @@ from instance_masks_from_images import image_text
 TEXT_EMBEDDING_SIZE = 512
 
 MESH = pv.read(r"C:\Users\sankl\Downloads\RA\RA_1M.ply")
-LOADED_MASKS = np.load(r'C:\Users\sankl\PycharmProjects\CityMagic3D\merge_masks\outputs\merge_masks\2024-05-24_23-50-30\merged_masks.npz')
-LOADED_TEXT_EMBEDDINGS = np.load(r'C:\Users\sankl\PycharmProjects\CityMagic3D\merge_masks\outputs\merge_masks\2024-05-24_23-50-30\merged_embeddings__avg.npz')
+LOADED_MASKS = np.load(r'C:\Users\sankl\PycharmProjects\CityMagic3D\merge_masks\outputs\merge_masks\2024-05-25_00-11-29\merged_masks.npz')
+LOADED_TEXT_EMBEDDINGS = np.load(r'C:\Users\sankl\PycharmProjects\CityMagic3D\merge_masks\outputs\merge_masks\2024-05-25_00-11-29\merged_embeddings__avg.npz')
 
-QUERY = "pool"
+#QUERY = "palm tree"
+#QUERY = "red car on a parking lot"
+QUERY = "roof of a residential house"
 
 mask_embeddings = {key: LOADED_MASKS[key] for key in LOADED_MASKS}
 
@@ -39,6 +41,6 @@ plotter.add_text(f"Query: '{QUERY}'", font_size=14)
 # plotter.subplot(0, 1)
 plotter.add_mesh(MESH, scalars='RGBA', rgb=True)
 # Add masks as points on top
-show_top_n_instances(plotter, MESH, scores, mask_embeddings, 30)
+show_top_n_instances(plotter, MESH, scores, mask_embeddings, 20)
 plotter.show()
 
